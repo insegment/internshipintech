a65169bc2aa3ef0661cfc6926c1568a34eade30d
Ovidiu Puscas <ovidiu.puscas@intechdynamics.com>
2016-06-13 18:04:33 +0300

    Prepare for wordpress install

--- intech-site/wp-content/themes/bubbles/sidebar.php
+++
@@ -1,4 +0,0 @@
-
-<?php if(!function_exists('dynamic_sidebar') || !dynamic_sidebar('main-sidebar')) :  ?>
-<a href="wp-admin/widgets.php"><?php echo __("Please Add Widget <a href='wp-admin/widgets.php'>here</a>","theme2035-fm") ?></a>
-<?php endif; ?>
