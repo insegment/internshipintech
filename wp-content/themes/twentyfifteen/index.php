86327d533ec896fb45454b6d283840228fe5fb30
Eugen PASCA <eugen.pasca@intechdynamics.com>
2016-06-15 12:11:01 +0300

    added the best  code in town

--- wp-content/themes/twentyfifteen/index.php
+++ wp-content/themes/twentyfifteen/index.php
@@ -9,9 +9,6 @@
  *
  * Learn more: {@link https://codex.wordpress.org/Template_Hierarchy}
  *
- * @package WordPress
- * @subpackage Twenty_Fifteen
- * @since Twenty Fifteen 1.0
  */
 
 get_header(); ?>
@@ -23,28 +20,21 @@ get_header(); ?>
 
 			<?php if ( is_home() && ! is_front_page() ) : ?>
 				<header>
-					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
+					<h1 class="page-title screen-reader-text">Cel mai șmecher  Titlu</h1>
 				</header>
 			<?php endif; ?>
 
 			<?php
 			// Start the loop.
 			while ( have_posts() ) : the_post();
+				get_template_part( 'content', 'post' );
 
-				/*
-				 * Include the Post-Format-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-				 */
-				get_template_part( 'content', get_post_format() );
-
-			// End the loop.
 			endwhile;
 
 			// Previous/next page navigation.
 			the_posts_pagination( array(
-				'prev_text'          => __( 'Previous page', 'twentyfifteen' ),
-				'next_text'          => __( 'Next page', 'twentyfifteen' ),
+				'prev_text'          => __( 'Previous', 'twentyfifteen' ),
+				'next_text'          => __( 'Next', 'twentyfifteen' ),
 				'before_page_number' => '<span class="meta-nav screen-reader-text">' . __( 'Page', 'twentyfifteen' ) . ' </span>',
 			) );
 
