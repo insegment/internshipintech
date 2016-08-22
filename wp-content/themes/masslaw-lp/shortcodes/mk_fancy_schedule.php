d03d12679050bbd8a96b6ad271d43a533d9d1373
Eugen PASCA <eugen.pasca@intechdynamics.com>
2016-08-22 12:15:01 +0300

    another fix

--- wp-content/themes/masslaw-lp/shortcodes/mk_fancy_schedule.php
+++ wp-content/themes/masslaw-lp/shortcodes/mk_fancy_schedule.php
@@ -1,20 +1,19 @@
 <?php
-$output = $el_class = '';
+$output = $visibility = $el_class = '';
 extract(shortcode_atts(array(
 	'visibility' => '',
     'el_class' => '',
 ), $atts));
 
 
-$output = '';
-$output .= '<div class="'.$el_class.' '.$visibility.' ">'
+$output .= '<div class="'.$el_class.' '.$visibility.' ">';
 	if(get_field('mslaw_schedule_list', 'option')):
 		while(has_sub_field('mslaw_schedule_list', 'option')):
-			$output .= '<div class="semester">'
+			$output .= '<div class="semester">';
 				$semester = get_sub_field('mslaw_schedule_list_semester');
 				$output .= '<h2 >'. $semester .'</h2>';
 				if( get_sub_field('mslaw_schedule_dates_list') ):
-					$output .= '<div class="schedule_list">'
+					$output .= '<div class="schedule_list">';
 					while ( has_sub_field('mslaw_schedule_dates_list') ) {
 						$attendance = get_sub_field('mslaw_schedule_date');
 						$output .= '<p >' . $attendance . '</p>';
